eecfe89f9d4c3b16c7c985ca9aaecafdf3df9d00
Mila9 <barbosa196milena@gmail.com>
2022-10-25 14:17:10 -0300

    adicionado projeto com implementação de herança, sobreescrevendo metodo, proriedades e parametros

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone(numero :"123456", modelo:"modelo 2", imei:"22222", memoria:64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+Nokia nokia = new Nokia(numero:"321567", modelo:"modelo 1", imei:"111111", memoria:32);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
